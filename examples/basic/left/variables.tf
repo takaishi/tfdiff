135338d6bb4851e157a992c9d72ce4c2c630fe39
Ryo Takaishi <ryo.takaishi.0@gmail.com>
2025-08-24 12:21:09 +0900

    chore: add variable that include list argument to example

--- examples/basic/left/variables.tf
+++ examples/basic/left/variables.tf
@@ -21,3 +21,9 @@ variable "vpc_cidr" {
   type        = string
   default     = "10.0.0.0/16"
 }
+
+variable "availability_zones" {
+  description = "List of availability zones to use"
+  type        = list(string)
+  default     = ["us-west-2a", "us-west-2b"]
+}
--- examples/basic/right/variables.tf
+++ examples/basic/right/variables.tf
@@ -30,3 +30,9 @@ variable "enable_alb" {
   type        = bool
   default     = true
 }
+
+variable "availability_zones" {
+  description = "List of availability zones to use"
+  type        = list(string)
+  default     = ["us-west-2a", "us-west-2b", "us-west-2c"]
+}
